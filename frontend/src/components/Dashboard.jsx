295a32994b50e2255c974855b55f34461b48ac31
tejaskalal <tejaskalal1125@gmail.com>
2025-11-29 23:55:40 +0530

    design user dashboard

--- frontend/src/components/Dashboard.jsx
+++
@@ -1,5 +0,0 @@
-const Dashboard = () => {
-  return <h1 className="text-center">Welcome to Dashboard</h1>;
-};
-
-export default Dashboard;
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,8 +1,45 @@
+import "./Profile.css";
+import {
+  FaUserCircle,
+  FaHeartbeat,
+  FaRunning,
+  FaAppleAlt,
+} from "react-icons/fa";
+
 const Profile = () => {
   return (
-    <h1 style={{ color: "black" }} className="text-center">
-      PROFILE PAGE WORKING
-    </h1>
+    <div className="container-user">
+      <div className="user">
+        <div className="user-greet">
+          <h1>Hello, User</h1>
+          <p>See your stats and take a step toward better health.</p>
+        </div>
+        <div className="user-info">
+          <FaUserCircle className="profile-icon" />
+          <h5>Tejas Kalal</h5>
+        </div>
+      </div>
+
+      <div className="card-container">
+        <div className="card">
+          <FaRunning className="card-icon" />
+          <h3>BMI</h3>
+          <p>Your BMI indicator</p>
+        </div>
+
+        <div className="card">
+          <FaHeartbeat className="card-icon" />
+          <h3>Health Score</h3>
+          <p>Improve health</p>
+        </div>
+
+        <div className="card">
+          <FaAppleAlt className="card-icon" />
+          <h3>Diet</h3>
+          <p>Check your nutrition score</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
